737d587fa47cf0cd3c0772aa785590327a9d052b
Zergen03 <alexizquierdo2003@gmail.com>
2025-08-12 14:07:20 +0200

    feat(services): add ITicketService and implementation

---
+++ Services/Interfaces/ITicketService.cs
@@ -0,0 +1,9 @@
+﻿using BarTasca.DTOs.Ticket;
+
+namespace BarTasca.Services.Interfaces;
+
+public interface ITicketService
+{
+    Task<TicketDetailDto> CreateAsync(CreateTicketDto dto, CancellationToken ct = default);
+    Task<TicketDetailDto?> GetAsync(int id, CancellationToken ct = default);
+}
---
+++ Services/Mapping/TicketProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BarTasca.Services.Mapping;
+
+public class TicketProfile : Profile
+{
+    public TicketProfile()
+    {
+        // TODO: mapear Ticket <-> TicketDto
+    }
+}
