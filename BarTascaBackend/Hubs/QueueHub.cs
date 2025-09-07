01b55b55f0f22e9e2c1c352196134377aa0c0813
Zergen03 <alexizquierdo2003@gmail.com>
2025-09-07 11:50:29 +0200

    eat(security): enforce CORS (dev/prod) and JWT for SignalR using access_token

--- BarTascaBackend/Hubs/QueueHub.cs
+++ BarTascaBackend/Hubs/QueueHub.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace BarTascaBackend.Hubs;
 
+[Authorize]
 public class QueueHub : Hub
 {
+    //TODO: Validar que el usuario tenga permisos para unirse a un grupo de ticket
     public Task JoinTicketGroup(int ticketId) =>
         Groups.AddToGroupAsync(Context.ConnectionId, $"ticket:{ticketId}");
 
     public Task LeaveTicketGroup(int ticketId) =>
         Groups.RemoveFromGroupAsync(Context.ConnectionId, $"ticket:{ticketId}");
 
+    [Authorize(Roles = "Admin,Worker")] // TODO: unificar en una policy "Staff"
     public Task JoinStaff() =>
         Groups.AddToGroupAsync(Context.ConnectionId, "staff");
 
